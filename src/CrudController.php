fab3178db7e33e18c52b8576a447e70177d96d7f
Ricardo R. Sierra <sierra.csi@gmail.com>
2022-07-07 08:14:49 -0300

    Fix bugs

--- src/CrudController.php
+++ src/CrudController.php
@@ -6,7 +6,7 @@
 namespace Pedreiro;
 
 use Illuminate\Database\Eloquent\Model;
-use Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Str;
